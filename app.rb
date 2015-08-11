1a26ada420f3bb1e91fc9e3d9f222bc437f45915
Lecky Lao <leckylao@gmail.com>
2015-08-11 16:36:03 +1000

    adds in redis support for broadcasting

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'faye/websocket'
+require 'redis'
 
 App = lambda do |env|
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
 
+    Thread.new do
+      Redis.new.subscribe 'chat' do |on|
+        on.message do |channel, message|
+          ws.send(message)
+        end
+      end
+    end
+
     ws.on :message do |event|
-      ws.send(event.data)
+      Redis.new.publish 'chat', event.data
+      p "Got #{event.data}"
+      # ws.send(event.data)
     end
 
     ws.on :close do |event|
